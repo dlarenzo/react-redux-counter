8fe907aa67fe4f223dee0dce298bfe1f59f246ea
LaRenzo <dlarenzo@outlook.com>
2024-02-09 13:04:28 -0700

    updated counters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { legacy_createStore as createStore } from "redux";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//REACT VERSION
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+// import { createStore } from "redux";
+
+// // STORE -> GLoBALIZED STATE
+
+// // ACTION -> DESCRIBES WHAT YOU WANT TO DO
+// // ACTION INCREMENT
+// const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
+
+// // ACTION REDUCER
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+// };
+
+// //CREATE STORE
+// let store = createStore(counter);
+
+// // Display it in the console
+// store.subscribe(() => console.log(store.getState()));
+
+// // DISPATCH -> EXECUTE THE ACTION
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/reducers/counter.js
@@ -0,0 +1,16 @@
+import { act } from "react-dom/test-utils";
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + action.payload;
+
+    case "DECREMENT":
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
